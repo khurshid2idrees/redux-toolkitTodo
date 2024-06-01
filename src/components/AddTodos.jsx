ce7e94565a6a62b26d00523660f2bc8c396a3539
khurshid idrees <umarkhurshid3@gmail.com>
2024-06-01 21:00:55 +0530

    update in main store  addtodos todos and todoslice

--- src/components/AddTodos.jsx
+++ src/components/AddTodos.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { addTodo } from "../features/todo/todoSlice";
+import { addTodo } from "../features/todoSlice";
 
 function AddTodos() {
-  const [input, setInput] = useState("");
+  const [text, setText] = useState("");
   const dispatch = useDispatch();
 
-  const addTodoHandler = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    dispatch(addTodo(input));
-    setInput("");
+    dispatch(addTodo(text));
+    setText("");
   };
 
   return (
-    <form className="space-x-3 mt-12" onSubmit={addTodoHandler}>
+    <form className="space-x-3 mt-12" onSubmit={handleSubmit}>
       <input
         type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         placeholder="Enter a Todo..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
       />
       <button
         type="submit"
---
+++ src/features/todoSlice.js
@@ -0,0 +1,23 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload
+      };
+      state.todos.push(todo)
+    },
+  },
+});
+
+export const { addTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
